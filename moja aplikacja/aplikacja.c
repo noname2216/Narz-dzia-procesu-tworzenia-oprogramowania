dac5f935b1648c3b3205b503654bb0f5b45ad394
noname2216 <mkulikowski26@gmail.com>
2023-06-27 20:34:13 +0200

    dokończono aplikację

--- moja aplikacja/aplikacja.c
+++ moja aplikacja/aplikacja.c
@@ -10,66 +10,100 @@ void koniec_lini()
 }
 
 //funkcja do zmiany kolejnośći zmiennych
-int zamiana()
+int zamiana(int a[],int i)
 {
-
+    int buf;
+    buf=a[i];
+    a[i]=a[i+1];
+    a[i+1]=buf;
 }
 
+int sortowanie_babelkowe(int tab[],int n)
+{
+    int i,k;
+    for(i=n-1;i>0;i--)
+    {
+        for(k=0;k<i;k++)
+        {
+            if (tab[k]>tab[k+1])
+            zamiana(tab[k],k);
+        }
+    }
+}
 int main ()
 {
-//do losowania liczb
-srand(time(0));
-//inty funkcji głównej
-int dlugosc,min,max;
+    //do losowania liczb
+    srand(time(0));
+    //inty funkcji głównej
+    int dlugosc,min,max;
 
-//początek programu
-printf("Początek programu");
-koniec_lini();
+    //początek programu
+    printf("Początek programu");
+    koniec_lini();
 
-//pobieranie danych od użytkownika
+    //pobieranie danych od użytkownika
 
-//dlugośc
-do
-{
-    printf("podaj długość tablicy");
-    scanf("%d",dlugosc);
-} while (dlugosc<0);
+    //dlugośc
+    do
+    {
+        printf("podaj długość tablicy");
+        scanf("%d",dlugosc);
+    } while (dlugosc<0);
 
-//tworzenie tablicy o podanej długośći
-int tab[dlugosc];
+    //tworzenie tablicy o podanej długośći
+    int tab[dlugosc];
 
-//minimum
-do
-{
-    printf("podaj minimum");
-    scanf("%d",min);
-} while (min<0);
+    //minimum
+    do
+    {
+        printf("podaj minimum");
+        scanf("%d",min);
+    } while (min<0);
 
 
-//maximum
-do
-{
-    printf("podaj maximum");
-    scanf("%d",max);
-} while (max<0);
+    //maximum
+    do
+    {
+        printf("podaj maximum");
+        scanf("%d",max);
+    } while (max<0);
 
-//losowanie liczb w tablicy
-for (int i = 0; i < dlugosc; i++)
-{
-    tab[i] = (rand() %(max - min + 1)) + min;
+    //losowanie liczb w tablicy
+    for (int i = 0; i < dlugosc; i++)
+    {
+        tab[i] = (rand() %(max - min + 1)) + min;
+    }
+
+    //wyświetlenie nieposotowanej tablicy
+    printf("przed sortowaniem");
+    koniec_lini();
+
+    for (int i = 0; i < dlugosc; i++)
+    {
+        printf("%d",tab[i]);
+    }
+
+    //sortowanie tablicy
+    sortowanie_babelkowe(tab,dlugosc);
+    //wyświetlenie posortowanej tablicy
+    printf("po sortowaniu");
+    koniec_lini();
+
+    for (int i = 0; i < dlugosc; i++)
+    {
+        printf("%d",tab[i]);
+    }
+
+    printf("wciśnij e by wyjść z programu");
+    //wyjście z programu
+    char exit;
+
+    exit = getchar();
+
+    if (exit == 'e' || exit == 'E')
+    {
+        return 0;
+    }
+    
+
 }
-
-//wyświetlenie nieposotowanej tablicy
-printf("przed sortowaniem");
-koniec_lini();
-
-for (int i = 0; i < dlugosc; i++)
-{
-    printf("%d",tab[i]);
-}
-
-//sortowanie tablicy
-
-}
-
-
